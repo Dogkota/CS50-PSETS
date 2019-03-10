fb4918345d28d0acff13638ab76336a9d3adb5e6
Dogkota <37309094+Dogkota@users.noreply.github.com>
2019-03-09 23:29:05 -0700

    Create mario.c
    PSET1 - mario.c

---
+++ mario.c
@@ -0,0 +1,28 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    do
+    {    
+        n = get_int("Height: ");
+    } while (n < 0 || n > 23);    // pyramid height must be between 0 and 23
+    
+    for (int i = 0; i < n; i++)    // iterate over loop n times
+    {
+        for (int j = 0; j < n - i - 1; j++)  // print (n - i - 1) spaces, increment j by 1 
+        {
+            printf(" ");  
+        }
+        for (int j = 0; j < i + 1; j++)  // print (i + 1) hashes, increment j by 1
+            printf("#");
+        {
+            printf("  "); // double space between pyramid halves
+        }
+        for (int j = 0; j < i + 1; j++)  // repeat hashes
+            printf("#");
+    
+    printf("\n");
+    }
+}
